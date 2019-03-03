1c9e88cfc212b69af45c279db8298f9eb63941dd
junan <junanyeap@gmail.com>
2019-03-03 17:28:21 +0800

    homework 2 : equation
    no comment yet, add it later

--- equation.py
+++ equation.py
@@ -2,20 +2,24 @@ import math
 
 def calcEquation(a,b,c):
     if a == 0:
-        print("單解")
+        # print("單解")
+        print("Single Solution")
         return -c/b
     elif b*b - 4 * a * c < 0:
         # 無解
-        print("無解")
+        # print("無解")
+        print("No Solution")
         return False
     elif b*b - 4 * a * c == 0:
         # 重根
-        print("重根")
+        # print("重根")
+        print("2 same root")
         return - b / ( 2 * a )
     else:
         # 兩相異實根
-        print("兩相異實根")
-        return (-b-math.sqrt(b*b-4*a*c))/(2*a),(-b+math.sqrt(b*b -4*a*c))/(2*a)
+        # print("兩相異實根")
+        print("2 different real root")
+        return (-b+math.sqrt(b*b-4*a*c))/(2*a),(-b-math.sqrt(b*b -4*a*c))/(2*a)
 
 def main():
     print("input a ax^2+bx+c :")
