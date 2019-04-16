4059eb69f67211c6fad2537d613020bb1b5eff60
junanyeap <junanyeap@gmail.com>
2019-04-16 10:09:55 +0800

    matrixRotate

---
+++ matrixRotate.py
@@ -0,0 +1,36 @@
+# [1,2,3]
+# [4,4,6]
+#      clockWise 90        ,     counterClockWise 90
+# [[4,1] , [5,2] , [6,3]]  ,   [[3,6] , [2,5] , [1,4]]
+# [row][column]
+# [[a[c-j-1][i] for j in range(r)] for i in range(c)]
+
+def matrixRotate(a):
+    c = 3
+    r = 2
+    # rotate clockWise 90
+    rc90 = [[a[r-j-1][i] for j in range(r)] for i in range(c)]
+    # rotate counterClockWise 90
+    rcc90 = [[a[j][c-i-1] for j in range(r)] for i in range(c)]
+    # rotate 180
+    r180 = [[a[r-i-1][c-j-1] for j in range(c)] for i in range(r)]
+    # flip
+    flip = [[a[r-i-1][j] for j in range(c)] for i in range(r)]
+    print("Result:")
+    for i in range(len(flip)):
+        print(flip[i])
+    
+    # for j in range(r):
+    #     for i in range(c):
+    #         print(a[r-j-1][i],end="")
+    #     print("")
+
+def main():
+    a = [[1,2,3],[4,5,6]]
+    print("OriMatrix:")
+    for i in range(len(a)):
+        print(a[i])
+    # a = input("input a list")
+    matrixRotate(a)
+
+main()
